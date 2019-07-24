adb2431a8f1246b5a165b78b71c483022bd5f7a2
Karen Flores <karen.ag.flores@gmail.com>
2019-07-24 20:03:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/item/)
       item_name = req.path.split("/item/").last
-      item = @@item.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
 
       resp.write "#{item.name} costs #{item.price}"
     else
