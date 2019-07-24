e4054a2223da2ed398cd0dcd4d4624fa5a8ea14d
Karen Flores <karen.ag.flores@gmail.com>
2019-07-24 20:03:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/item/)
       item_name = req.path.split("/item/").last
       item = @@item.find{|i| i.name == item_name}
-      
+
       resp.write "#{item.name} costs #{item.price}"
     else
       resp.write "Route not found"
